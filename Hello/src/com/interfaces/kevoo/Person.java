1504f3c2f814b9707889731c38cfedf3dbf2bd4c
charity1475 <charitymbis@gmail.com>
2020-08-26 22:38:55 +0300

    Update Person.java

--- Hello/src/com/interfaces/kevoo/Person.java
+++ Hello/src/com/interfaces/kevoo/Person.java
@@ -1,5 +1,16 @@
 package com.interfaces.kevoo;
 
-public abstract class Person {
+public class Person {
+	public static void main(String[] args) {
+		Vegan john = new Vegan() {
+			
+			@Override
+			void speak() {
+				System.out.println("John speak Vegan languages");
+			}
+		};
+		john.eat();
+		john.speak();
+	}
 
 }
