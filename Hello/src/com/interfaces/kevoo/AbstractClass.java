49874df43ad38183a3eed0c8567ea477ddea78f6
charity1475 <charitymbis@gmail.com>
2020-08-26 22:16:15 +0300

    Update AbstractClass.java

--- Hello/src/com/interfaces/kevoo/AbstractClass.java
+++ Hello/src/com/interfaces/kevoo/AbstractClass.java
@@ -1,8 +1,10 @@
 package com.interfaces.kevoo;
 
-public class AbstractClass {
-	public static void main(String[] args) {
-		
+public abstract class AbstractClass {
+	public static void speak() {
+		System.out.println("No free lunch in America");
 	}
+	/*an abstract method */
+	public abstract void eat();
 
 }
