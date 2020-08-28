4ca77553038f3c678e582018e0d88c1654059dd4
charity1475 <charitymbis@gmail.com>
2020-08-28 08:10:03 +0300

    Update Both.java

---
+++ Hello/src/com/multipleinheritance/kevoo/Both.java
@@ -0,0 +1,12 @@
+package com.multipleinheritance.kevoo;
+
+public class Both {
+
+	public static void main(String[] args) {
+		Person man = new Person();
+		man.breathe();
+		man.message();
+
+	}
+
+}
