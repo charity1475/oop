f6fa8e291ad94a8773d60ed6db56d0981dbb026d
charity1475 <charitymbis@gmail.com>
2020-08-28 08:50:16 +0300

    Update Door.java

--- Hello/src/com/oop/kevoo/shop/Door.java
+++ Hello/src/com/oop/kevoo/shop/Door.java
@@ -1,5 +1,40 @@
 package com.oop.kevoo.shop;
 
 public class Door {
+	public Lock lock;
+	/*Door constructor */
+	public Door() {
+		lock = new Lock(true);
+	}
+	public void shopStatus() {
+		if(lock.isLock()) {
+			System.out.println("The shop is now closed");
+		}
+		else {
+			System.out.println("The shop is open Welcome");
+		}
+	}
+	
+
+	public Lock getLock() {
+		return lock;
+	}
+
+
+	public class Lock{
+		private boolean lock;
+		/*constructor for nested class*/
+		public Lock(boolean lock) {
+			this.lock = lock;
+		}
+		/*Getters and setters*/
+		public boolean isLock() {
+			return lock;
+		}
+		public void setLock(boolean lock) {
+			this.lock = lock;
+		}
+		
+	}
 
 }
