b7d639d8140c0ccc80ad613721e4f1529ccaf12f
charity1475 <charitymbis@gmail.com>
2020-08-28 08:50:21 +0300

    Update Shop.java

--- Hello/src/com/oop/kevoo/Shop.java
+++ Hello/src/com/oop/kevoo/Shop.java
@@ -1,5 +1,13 @@
 package com.oop.kevoo;
 
+import com.oop.kevoo.shop.Door;
+
 public class Shop {
+	public static void main(String[] args) {
+		Door door = new Door();
+		door.shopStatus();
+		door.getLock().setLock(false);
+		door.shopStatus();
+	}
 
 }
