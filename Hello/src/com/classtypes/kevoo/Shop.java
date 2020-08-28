dbaddc554f4318b8a612efb6ddecd0fe75bae7ca
charity1475 <charitymbis@gmail.com>
2020-08-28 11:28:44 +0300

    Update Shop.java

--- Hello/src/com/classtypes/kevoo/Shop.java
+++ Hello/src/com/classtypes/kevoo/Shop.java
@@ -3,7 +3,13 @@ package com.classtypes.kevoo;
 public class Shop {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Door door = new Door();
+		if(door.isLocked(args)) {
+			System.out.println("The shop is closed ");
+		}
+		else {
+			System.out.println("Welcome the shop is open ");
+		}
 
 	}
 
