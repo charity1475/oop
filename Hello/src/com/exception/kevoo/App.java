d6d7ea46a4a6eda07b7c1b3d663d414ce5362b1f
charity1475 <charitymbis@gmail.com>
2020-08-29 18:00:59 +0300

    Finally block

--- Hello/src/com/exception/kevoo/App.java
+++ Hello/src/com/exception/kevoo/App.java
@@ -3,7 +3,7 @@ package com.exception.kevoo;
 public class App {
 
 	public static void main(String[] args) {
-		case1(8,0);
+		case1(0,8);
 		case2(8,2);
 
 	}
@@ -16,7 +16,12 @@ public class App {
 			System.out.println("Zero Division error");
 			e.printStackTrace();
 		}
-		
+		/*finally block will always be executed*/
+		try {
+			System.out.println("Reversed case1: "+ x/y);
+		} finally {
+			System.out.println("Finally on air");
+		}
 	}
 	public static void case2(int x, int y) {
 		System.out.println("case2: x-y = " + (x-y));
