4ced61ff3ff5a0803846280cf158bbcbd4e60279
charity1475 <charitymbis@gmail.com>
2020-08-29 18:21:40 +0300

    added a block

--- Hello/src/com/exception/kevoo/Main.java
+++ Hello/src/com/exception/kevoo/Main.java
@@ -1,5 +1,8 @@
 package com.exception.kevoo;
 
+import java.io.File;
+import java.io.FileReader;
+
 public class Main {
 
 	public static void main(String[] args) {
@@ -7,6 +10,12 @@ public class Main {
 	}
 	public static void method() {
 		System.out.println("Hello, I'm the method");
+		try {
+			FileReader reader = new FileReader("file.txt");
+			reader.close();
+		} catch (Exception e) {
+			System.out.println("The file doesn't exist");
+		}
 	}
 
 }
