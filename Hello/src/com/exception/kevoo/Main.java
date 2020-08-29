05b46e63ba808e8cf0a69bc7b26ec6a3adf19f56
charity1475 <charitymbis@gmail.com>
2020-08-29 18:38:23 +0300

    exceptions

--- Hello/src/com/exception/kevoo/Main.java
+++ Hello/src/com/exception/kevoo/Main.java
@@ -1,21 +1,23 @@
 package com.exception.kevoo;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 
 public class Main {
 
-	public static void main(String[] args) {
-		method();
-	}
-	public static void method() {
-		System.out.println("Hello, I'm the method");
+	public static void main(String[] args){
 		try {
-			FileReader reader = new FileReader("file.txt");
-			reader.close();
+			method();
 		} catch (Exception e) {
-			System.out.println("The file doesn't exist");
+			System.out.println("Exception");
 		}
+	}
+	public static void method() throws FileNotFoundException {
+		System.out.println("Hello, I'm the method");
+			FileReader reader = new FileReader("file.txt");
+			throw new FileNotFoundException();
+		
 	}
 
 }
