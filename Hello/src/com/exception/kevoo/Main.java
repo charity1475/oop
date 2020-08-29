92515efebd6589dc59d79334e6bc2299fdc1572a
charity1475 <charitymbis@gmail.com>
2020-08-29 18:16:45 +0300

    throw and throws

---
+++ Hello/src/com/exception/kevoo/Main.java
@@ -0,0 +1,12 @@
+package com.exception.kevoo;
+
+public class Main {
+
+	public static void main(String[] args) {
+		method();
+	}
+	public static void method() {
+		System.out.println("Hello, I'm the method");
+	}
+
+}
