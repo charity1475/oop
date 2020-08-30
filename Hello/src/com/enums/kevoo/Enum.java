5c74b9aafc232e01393274b90d959b5a470c9bf6
charity1475 <charitymbis@gmail.com>
2020-08-30 11:54:44 +0300

    enums

---
+++ Hello/src/com/enums/kevoo/Enum.java
@@ -0,0 +1,5 @@
+package com.enums.kevoo;
+
+public enum Enum {
+
+}
--- Hello/src/com/exception/kevoo/Main.java
+++ Hello/src/com/exception/kevoo/Main.java
@@ -2,33 +2,25 @@ package com.exception.kevoo;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
-class UserDefinedException extends Throwable{
-	
-}
 
-public class Main extends Exception{
+public class Main{
 
 	public static void main(String[] args){
 		try {
 			method();
 		}catch (FileNotFoundException e) {
 			System.out.println("File not found Exception");
-		} catch (UserDefinedException e) {
-			System.out.println("My Exception");
-		}catch (Exception e) {
+		} catch (Exception e) {
 			System.out.println("Exception");
 		}
 	}
 	// child class can not handle a parent exception
-	public static void method() throws Exception,FileNotFoundException, UserDefinedException {
+	public static void method() throws Exception,FileNotFoundException {
 		System.out.println("Hello, I'm the method");
 			FileReader reader = new FileReader("file.txt");
 			switch (2) {
 			case 1: 
 				throw new FileNotFoundException();
-				
-			case 2: 
-				throw new UserDefinedException();
 						
 			default:
 				throw new Exception();
